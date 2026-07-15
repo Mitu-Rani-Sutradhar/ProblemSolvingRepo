1e141784fcf0c0cdde2003b9afb659bf244442c0
Mitu-Rani-Sutradhar <mitusutradhar4965@gmail.com>
2026-07-15 14:08:12 +0600

    problem2

---
+++ problem2.js
@@ -0,0 +1,10 @@
+// Return "Even" if a number is even, otherwise "Odd".
+
+function checkEvenOdd(num){
+   if(num % 2 === 0){
+     return "Even";
+   }
+   else{
+    return "Odd";   }
+}
+console.log(checkEvenOdd(4)); 
