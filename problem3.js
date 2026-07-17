b8cebeaf1f81836fc8e9e8ccbbe76ee4f3bef9af
Mitu-Rani-Sutradhar <mitusutradhar4965@gmail.com>
2026-07-17 12:19:50 +0600

    problem4

--- problem3.js
+++ problem3.js
@@ -1,6 +1,6 @@
 // Find the Largest Number
 function findLargestNumber(arr){
-    let largest = arr[0];
+    let largest = arr[0];q2v
     
     for(let i = 1; i < arr.length; i++){
         if(arr[i] > largest){
---
+++ problem4.js
@@ -0,0 +1,8 @@
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  }
+  return "Odd";
+}
+
+console.log(checkEvenOdd(7));
